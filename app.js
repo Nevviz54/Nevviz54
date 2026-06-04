d95083d3de5de75923cff42791fb7fb0d9821b55
Seu Nome <seu-email-do-github@email.com>
2026-06-04 15:08:35 -0300

    erro resolvido!

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
-function exibirMensagemInicial() {
+function exibirMensagemInicial() {}
 
     exibirTextoNaTela('h1', 'Jogo do número secreto 2');
 
