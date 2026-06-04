134a9aa5944551227f63b7b18b3dbd7f7d1dbfde
Seu Nome <seu-email-do-github@email.com>
2026-06-04 14:40:53 -0300

    Troca da quantidade de numeros.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 62;
+let numeroLimite = 200;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirMensagemInicial() {
 
     exibirTextoNaTela('h1', 'Jogo do número secreto 2');
 
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 62');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 200');
 }
 
 exibirMensagemInicial();
